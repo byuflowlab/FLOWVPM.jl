e6484782622eac1181828715be54c1d0e71ef6b6
EdoAlvarezR <Edo.AlvarezR@gmail.com>
2019-02-28 17:37:40 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# FLOWVPM
+
+Implementation of the viscous Vortex Particle Method written in Julia 1.0.
+
+## Features
+  * Single node CPU parallel processing (threads).
+  * Single node GPU parallel processing.
+  * Automatic differentiation
+    * ForwardDiff: Tested on CPU. Capable of working on GPU if enough cache memory is available
+
+## Folders
+  * `src`         : Source code.
+  * `examples`    : Validation cases and examples of how to use this code.
+
+## Notes
+  * Parallel processing is done through [multi-threading](https://docs.julialang.org/en/v1.0/manual/parallel-computing/#Multi-Threading-(Experimental)-1), which requires declaring the number of threads (or cores available) through the system environment variable `JULIA_NUM_THREADS` that Julia reads before launching. To do so, either do `export JULIA_NUM_THREADS=4` before launching Julia, or add this line (after changing the number of threads according to the number of cores aviable to your machine) to the system environment (i.e., `~/.bashrc` and/or `~/.bash_profile`). Verify the correct declaration of threads with `Threads.nthreads()` after launching Julia.
+
+## Copyright
+Copyrighted to Eduardo J. Alvarez. All rights reserved. No public license, use, or modification of this code without consent.
---
+++ src/FLOWVPM.jl
@@ -0,0 +1,73 @@
+"""
+  Implementation of the three-dimensional viscous Vortex Particle Method written in Julia 1.0.
+
+  # AUTHORSHIP
+    * Author    : Eduardo J Alvarez
+    * Email     : Edo.AlvarezR@gmail.com
+    * Created   : Feb 2019
+    * Copyright : Eduardo J Alvarez. All rights reserved.
+"""
+module FLOWVPM
+
+# ------------ EXPOSED FUNCTIONS -----------------------------------------------
+# export Particle, stretching, vorticity
+# export ParticleField, addparticle, nextstep
+# export Kernel, kernel_sing, kernel_gauserf, kernel_gaus, kernel_wnklmns
+# export UJ_directAD, UJ_direct, UJ_directSym, UJ_fmm
+# export U, omega_approx
+# export FMM, Cell, buildTree, init_cells, upwardPass, horizontalPass, downwardPass
+# export run_vpm!, save, load_particlefield
+
+# ------------ GENERIC MODULES -------------------------------------------------
+import Dates
+import HDF5
+import SpecialFunctions
+import ForwardDiff
+FD = ForwardDiff
+using LinearAlgebra
+using Distributed
+
+# MultiComplex operations
+import Base.*
+import Base.conj
+import Base.real
+import Base.imag
+import Base.atan
+
+
+# ------------ FLOW MODULES ----------------------------------------------------
+# None required
+
+# ------------ GLOBAL VARIABLES ------------------------------------------------
+const module_path = splitdir(@__FILE__)[1]      # Path to this module
+
+const ADFLAG = true
+
+# This line defines a concrete real for type definitions
+if ADFLAG
+  println("******** AD Activated ******************")
+  const CReal = FD.Dual{Nothing,Float64,3}    # Use this for automatic derivatives
+else
+  println("******** AD Deactivated ******************")
+  const CReal = Float64                       # Use this for fast performance
+end
+
+# ------------ HEADERS ---------------------------------------------------------
+for header_name in []
+  include("FLOWVPM_"*header_name*".jl")
+end
+
+
+
+# ------------ MODULE PROPERTIES -----------------------------------------------
+# Constant values
+const EPS = 1e-15                     # Epsilon
+const H_CSDA = 1e-32                  # Complex step
+
+# Available Kernels
+# const kernel_sing = Kernel(zeta_sing, g_sing, dgdr_sing, g_dgdr_sing)
+# const kernel_gauserf = Kernel(zeta_gauserf, g_gauserf, dgdr_gauserf, g_dgdr_gauserf)
+# const kernel_gaus = Kernel(zeta_gaus, g_gaus, dgdr_gaus, g_dgdr_gaus)
+# const kernel_wnklmns = Kernel(zeta_wnklmns, g_wnklmns, dgdr_wnklmns, g_dgdr_wnklmns)
+
+end # END OF MODULE
