ad595e633330fdeeb9edbea302015e863839f6d4
cibinjoseph <cibinjoseph92@gmail.com>
2024-03-29 00:28:24 -0600

    Convert direct to use getter functions

--- src/FLOWVPM_UJ.jl
+++ src/FLOWVPM_UJ.jl
@@ -61,56 +61,56 @@ function UJ_direct(sources, targets, g_dgdr::Function, zeta, toggle_sfs, transpo
     for Pi in iterate(targets)
         for Pj in iterate(sources)
 
-            dX1 = Pi[1] - Pj[1]
-            dX2 = Pi[2] - Pj[2]
-            dX3 = Pi[3] - Pj[3]
+            dX1 = get_X(Pi)[1] - get_X(Pj)[1]
+            dX2 = get_X(Pi)[2] - get_X(Pj)[2]
+            dX3 = get_X(Pi)[3] - get_X(Pj)[3]
             r2 = dX1*dX1 + dX2*dX2 + dX3*dX3
 
             if !iszero(r2)
                 r = sqrt(r2) 
 
                 # Regularizing function and deriv
-                g_sgm, dg_sgmdr = g_dgdr(r/Pj[7])
+                g_sgm, dg_sgmdr = g_dgdr(r/get_sigma(Pj)[])
 
                 # K × Γp
-                crss1 = -const4 / r^3 * ( dX2*Pj[6] - dX3*Pj[5] )
-                crss2 = -const4 / r^3 * ( dX3*Pj[4] - dX1*Pj[6] )
-                crss3 = -const4 / r^3 * ( dX1*Pj[5] - dX2*Pj[4] )
+                crss1 = -const4 / r^3 * ( dX2*get_Gamma(Pj)[3] - dX3*get_Gamma(Pj)[2] )
+                crss2 = -const4 / r^3 * ( dX3*get_Gamma(Pj)[1] - dX1*get_Gamma(Pj)[3] )
+                crss3 = -const4 / r^3 * ( dX1*get_Gamma(Pj)[2] - dX2*get_Gamma(Pj)[1] )
 
                 # U = ∑g_σ(x-xp) * K(x-xp) × Γp
-                Pi[10] += g_sgm * crss1
-                Pi[11] += g_sgm * crss2
-                Pi[12] += g_sgm * crss3
+                get_U(Pi)[1] += g_sgm * crss1
+                get_U(Pi)[2] += g_sgm * crss2
+                get_U(Pi)[3] += g_sgm * crss3
 
                 # ∂u∂xj(x) = ∑[ ∂gσ∂xj(x−xp) * K(x−xp)×Γp + gσ(x−xp) * ∂K∂xj(x−xp)×Γp ]
                 # ∂u∂xj(x) = ∑p[(Δxj∂gσ∂r/(σr) − 3Δxjgσ/r^2) K(Δx)×Γp
-                aux = dg_sgmdr/(Pj[7]*r) - 3*g_sgm /r^2
+                aux = dg_sgmdr/(get_sigma(Pj)[]*r) - 3*g_sgm /r^2
                 # j=1
-                Pi[16] += aux * crss1 * dX1
-                Pi[17] += aux * crss2 * dX1
-                Pi[18] += aux * crss3 * dX1
+                get_J(Pi)[1] += aux * crss1 * dX1
+                get_J(Pi)[2] += aux * crss2 * dX1
+                get_J(Pi)[3] += aux * crss3 * dX1
                 # j=2
-                Pi[19] += aux * crss1 * dX2
-                Pi[20] += aux * crss2 * dX2
-                Pi[21] += aux * crss3 * dX2
+                get_J(Pi)[4] += aux * crss1 * dX2
+                get_J(Pi)[5] += aux * crss2 * dX2
+                get_J(Pi)[6] += aux * crss3 * dX2
                 # j=3
-                Pi[22] += aux * crss1 * dX3
-                Pi[23] += aux * crss2 * dX3
-                Pi[24] += aux * crss3 * dX3
+                get_J(Pi)[7] += aux * crss1 * dX3
+                get_J(Pi)[8] += aux * crss2 * dX3
+                get_J(Pi)[9] += aux * crss3 * dX3
 
                 # ∂u∂xj(x) = −∑gσ/(4πr^3) δij×Γp
                 # Adds the Kronecker delta term
                 aux = - const4 * g_sgm / r^3
 
                 # j=1
-                Pi[17] -= aux * Pj[6]
-                Pi[18] += aux * Pj[5]
+                get_J(Pi)[2] -= aux * get_Gamma(Pj)[3]
+                get_J(Pi)[3] += aux * get_Gamma(Pj)[2]
                 # j=2
-                Pi[19] += aux * Pj[6]
-                Pi[21] -= aux * Pj[4]
+                get_J(Pi)[4] += aux * get_Gamma(Pj)[3]
+                get_J(Pi)[6] -= aux * get_Gamma(Pj)[1]
                 # j=3
-                Pi[22] -= aux * Pj[5]
-                Pi[23] += aux * Pj[4]
+                get_J(Pi)[7] -= aux * get_Gamma(Pj)[2]
+                get_J(Pi)[8] += aux * get_Gamma(Pj)[1]
 
             end
             if toggle_sfs
