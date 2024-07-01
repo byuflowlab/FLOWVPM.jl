71c7a7a1e9fd441af64177d09ea50b88e90ce837
flowlab <rymanderson@gmail.com>
2024-07-01 04:41:54 -0600

    No changes

--- src/FLOWVPM_kernel.jl
+++ src/FLOWVPM_kernel.jl
@@ -36,7 +36,7 @@ const sqr2 = sqrt(2)
 
 # Newtonian velocity kernel
 # Knew(X) = -const4 * X / norm(X)^3
-function Knew(X)
+@inline function Knew(X)
     aux = -const4 / (X[1]^2 + X[2]^2 + X[3]^2)^1.5
     return (aux*X[1], aux*X[2], aux*X[3])
 end
@@ -45,22 +45,22 @@ end
 zeta_sing(r) = iszero(r) ? 1.0 : 0.0
 g_sing(r) = 1.0
 dgdr_sing(r) = 0.0
-g_dgdr_sing(r) = (g_sing(r), dgdr_sing(r))
+@inline g_dgdr_sing(r) = (1.0, 0.0)
 
 # erf Gaussian kernel
 zeta_gauserf(r) = const1*exp(-r^2/2)
-g_gauserf(r) = SpecialFunctions.erf(r/sqr2) - const2*r*exp(-r^2/2)
+g_gauserf(r) = custom_erf(r/sqr2) - const2*r*exp(-r^2/2)
 dgdr_gauserf(r) = const2*r^2*exp(-r^2/2)
-function g_dgdr_gauserf(r)
+@inline function g_dgdr_gauserf(r)
   aux = const2*r*exp(-r^2/2)
-  return SpecialFunctions.erf(r/sqr2)-aux, r*aux
+  return custom_erf(r/sqr2)-aux, r*aux
 end
 
 # Gaussian kernel
 zeta_gaus(r) = const3*exp(-r^3)
 g_gaus(r) = 1-exp(-r^3)
 dgdr_gaus(r) = 3*r^2*exp(-r^3)
-function g_dgdr_gaus(r)
+@inline function g_dgdr_gaus(r)
   aux = exp(-r^3)
   return 1-aux, 3*r^2*aux
 end
@@ -69,9 +69,7 @@ end
 zeta_wnklmns(r) = const4 * 7.5 / (r^2 + 1)^3.5
 g_wnklmns(r) = r^3 * (r^2 + 2.5) / (r^2 + 1)^2.5
 dgdr_wnklmns(r) = 7.5 * r^2 / (r^2 + 1)^3.5
-function g_dgdr_wnklmns(r)
+@inline function g_dgdr_wnklmns(r)
   aux0 = (r^2 + 1)^2.5
-
-  # Returns g, dgdr
   return r^3 * (r^2 + 2.5) / aux0, 7.5 * r^2 / (aux0*(r^2 + 1))
 end
