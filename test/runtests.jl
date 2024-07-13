bf7de7ca24f70991539c7a32642d28bf32c98b14
Cibin Joseph <cibin@login01.rc.byu.edu>
2024-07-12 21:55:51 -0600

    Fix tests for numerical useGPU

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,13 +9,13 @@ import FLOWVPM
 using FLOWVPM.CUDA
 
 # Run tests on CPU
-const test_using_GPU = fill(false)
+const test_using_GPU = fill(0)
 include("runtests_singlevortexring.jl")
 include("runtests_leapfrog.jl")
 
 # Run tests on GPU if device is functional
 if CUDA.functional()
-    test_using_GPU[] = true
+    test_using_GPU[] = 1
     include("runtests_singlevortexring.jl")
     include("runtests_leapfrog.jl")
 end
