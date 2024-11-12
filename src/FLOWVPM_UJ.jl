65a3a3f0938b4bc4794c80f4f07fceca97ca1f65
Ryan Anderson <rymanderson@gmail.com>
2024-11-12 16:38:34 -0700

    update FastMultipole gpu syntax

--- src/FLOWVPM_UJ.jl
+++ src/FLOWVPM_UJ.jl
@@ -92,7 +92,7 @@ function UJ_fmm(
     #@show typeof(pfield)
 
     # Calculate FMM of vector potential
-    fmm.fmm!(pfield; expansion_order=fmm_options.p-1, leaf_size=fmm_options.ncrit, multipole_threshold=fmm_options.theta, nearfield=true, farfield=farfield, unsort_bodies=sort, shrink_recenter=fmm_options.nonzero_sigma, lamb_helmholtz=true, gpu=(useGPU>0))
+    fmm.fmm!(pfield; expansion_order=fmm_options.p-1, leaf_size=fmm_options.ncrit, multipole_threshold=fmm_options.theta, nearfield=true, farfield=farfield, unsort_bodies=sort, shrink_recenter=fmm_options.nonzero_sigma, lamb_helmholtz=true, nearfield_device=(useGPU>0))
     # This should be concurrent_direct=(pfield.useGPU > 0)
     # But until multithread_direct!() works for the target_indices argument,
     # we'll leave it true
