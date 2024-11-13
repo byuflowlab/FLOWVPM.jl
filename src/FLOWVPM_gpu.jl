2fb4c5b1a5e6e089a127d348be01b6b6a57ee0b1
cibinjoseph <cibinjoseph92@gmail.com>
2024-11-12 17:11:38 -0700

    Correct error in shared memory check function

--- src/FLOWVPM_gpu.jl
+++ src/FLOWVPM_gpu.jl
@@ -11,7 +11,7 @@ end
 
 function check_shared_memory(dev, shmem_required, throw_error=true)
     dev_shmem = CUDA.attribute(dev, CUDA.DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK)
-    if shmem > dev_shmem
+    if shmem_required > dev_shmem
         msg = "Shared memory requested ($shmem B), exceeds available space ($dev_shmem B) on GPU. Try reducing ncrit, using more GPUs or reduce Chunk size if using ForwardDiff."
         if throw_error
             error(msg)
@@ -70,149 +70,144 @@ end
 
 const eps2 = 1e-6
 const const4 = 0.25/pi
-@inline function gpu_interaction(tx, ty, tz, s, j, kernel)
+@inline function gpu_interaction!(UJ, tx, ty, tz, s, j, kernel)
     T = eltype(s)
-    @inbounds dX1 = tx - s[1, j]
-    @inbounds dX2 = ty - s[2, j]
-    @inbounds dX3 = tz - s[3, j]
-    r2 = dX1*dX1 + dX2*dX2 + dX3*dX3
+    @inbounds dX1 = tx - s[1i32, j]
+    @inbounds dX2 = ty - s[2i32, j]
+    @inbounds dX3 = tz - s[3i32, j]
+    r2 = dX1^2 + dX2^2 + dX3^2
     r = sqrt(r2)
-    r3 = r*r2
 
     # Mapping to variables
-    @inbounds gam1 = s[4, j]
-    @inbounds gam2 = s[5, j]
-    @inbounds gam3 = s[6, j]
     @inbounds sigma = s[7, j]
 
-    UJ = @MVector zeros(T, 12)
-
     if r2 > T(eps2) && abs(sigma) > T(eps2)
+        # Mapping to variables
+        c4 = -T(const4)/(r*r2)
+        @inbounds gam1 = c4 * s[4i32, j]
+        @inbounds gam2 = c4 * s[5i32, j]
+        @inbounds gam3 = c4 * s[6i32, j]
+
         # Regularizing function and deriv
         # g_sgm = g_val(r/sigma)
         # dg_sgmdr = dg_val(r/sigma)
         g_sgm, dg_sgmdr = kernel(r/sigma)
 
-        # K × Γp
-        crss1 = -T(const4) / r3 * ( dX2*gam3 - dX3*gam2 )
-        crss2 = -T(const4) / r3 * ( dX3*gam1 - dX1*gam3 )
-        crss3 = -T(const4) / r3 * ( dX1*gam2 - dX2*gam1 )
-
-        # U = ∑g_σ(x-xp) * K(x-xp) × Γp
-        @inbounds UJ[1] = g_sgm * crss1
-        @inbounds UJ[2] = g_sgm * crss2
-        @inbounds UJ[3] = g_sgm * crss3
-
         # ∂u∂xj(x) = ∑[ ∂gσ∂xj(x−xp) * K(x−xp)×Γp + gσ(x−xp) * ∂K∂xj(x−xp)×Γp ]
         # ∂u∂xj(x) = ∑p[(Δxj∂gσ∂r/(σr) − 3Δxjgσ/r^2) K(Δx)×Γp
         aux = dg_sgmdr/(sigma*r) - 3*g_sgm /r2
+
+        # K × Γp
+        crss1 = dX2*gam3 - dX3*gam2
+        crss2 = dX3*gam1 - dX1*gam3
+        crss3 = dX1*gam2 - dX2*gam1
+
+        # U = ∑g_σ(x-xp) * K(x-xp) × Γp
+        @inbounds UJ[1i32] += g_sgm * crss1
+        @inbounds UJ[2i32] += g_sgm * crss2
+        @inbounds UJ[3i32] += g_sgm * crss3
+
+        @inbounds gam1 *= g_sgm
+        @inbounds gam2 *= g_sgm
+        @inbounds gam3 *= g_sgm
+
         # ∂u∂xj(x) = −∑gσ/(4πr^3) δij×Γp
         # Adds the Kronecker delta term
-        aux2 = -T(const4) * g_sgm / r3
         # j=1
-        @inbounds UJ[4] = aux * crss1 * dX1
-        @inbounds UJ[5] = aux * crss2 * dX1 - aux2 * gam3
-        @inbounds UJ[6] = aux * crss3 * dX1 + aux2 * gam2
+        @inbounds UJ[4i32] += aux * crss1 * dX1
+        @inbounds UJ[5i32] += aux * crss2 * dX1 - gam3
+        @inbounds UJ[6i32] += aux * crss3 * dX1 + gam2
         # j=2
-        @inbounds UJ[7] = aux * crss1 * dX2 + aux2 * gam3
-        @inbounds UJ[8] = aux * crss2 * dX2
-        @inbounds UJ[9] = aux * crss3 * dX2 - aux2 * gam1
+        @inbounds UJ[7i32] += aux * crss1 * dX2 + gam3
+        @inbounds UJ[8i32] += aux * crss2 * dX2
+        @inbounds UJ[9i32] += aux * crss3 * dX2 - gam1
         # j=3
-        @inbounds UJ[10] = aux * crss1 * dX3 - aux2 * gam2
-        @inbounds UJ[11] = aux * crss2 * dX3 + aux2 * gam1
-        @inbounds UJ[12] = aux * crss3 * dX3
+        @inbounds UJ[10i32] += aux * crss1 * dX3 - gam2
+        @inbounds UJ[11i32] += aux * crss2 * dX3 + gam1
+        @inbounds UJ[12i32] += aux * crss3 * dX3
     end
 
-    return UJ
+    return
 end
 
 # Each thread handles a single target and uses local GPU memory
 # Sources divided into multiple columns and influence is computed by multiple threads
-function gpu_atomic_direct!(s, t, p, num_cols, kernel)
+function gpu_atomic_direct!(s, t, p, q, kernel)
     t_size::Int32 = size(t, 2)
     s_size::Int32 = size(s, 2)
 
     ithread::Int32 = threadIdx().x
 
     # Row and column indices of threads in a block
-    row::Int32 = (ithread-1) % p + 1
-    col::Int32 = floor(Int32, (ithread-1)/p) + 1
+    row::Int32 = (ithread-1i32) % p + 1i32
+    col::Int32 = floor(Int32, (ithread-1i32)/p) + 1i32
 
-    itarget::Int32 = row + (blockIdx().x-1)*p
+    itarget::Int32 = row + (blockIdx().x-1i32)*p
     if itarget <= t_size
-        @inbounds tx = t[1, itarget]
-        @inbounds ty = t[2, itarget]
-        @inbounds tz = t[3, itarget]
+        @inbounds tx = t[1i32, itarget]
+        @inbounds ty = t[2i32, itarget]
+        @inbounds tz = t[3i32, itarget]
     end
 
     n_tiles::Int32 = CUDA.ceil(Int32, s_size / p)
-    bodies_per_col::Int32 = CUDA.ceil(Int32, p / num_cols)
+    bodies_per_col::Int32 = CUDA.ceil(Int32, p / q)
 
-    # 12 for UJ variables that have to be reduced at the end
     sh_mem = CuDynamicSharedArray(eltype(t), (7, p))
-    # sh_mem = CuDynamicSharedArray(eltype(t), (12*p, p))
 
     # Variable initialization
     UJ = @MVector zeros(eltype(t), 12)
-    out = @MVector zeros(eltype(t), 12)
     idim::Int32 = 0
-    i::Int32 = 0
     isource::Int32 = 0
+    i::Int32 = 0
 
     itile::Int32 = 1
     while itile <= n_tiles
         # Each thread will copy source coordinates corresponding to its index into shared memory. This will be done for each tile.
-        if (col == 1)
-            isource = row + (itile-1)*p
-            idim = 1
+        if (col == 1i32)
+            isource = row + (itile-1i32)*p
+            idim = 1i32
             if isource <= s_size
-                while idim <= 7
+                while idim <= 7i32
                     @inbounds sh_mem[idim, row] = s[idim, isource]
-                    idim += 1
+                    idim += 1i32
                 end
             else
-                while idim <= 7
+                while idim <= 7i32
                     @inbounds sh_mem[idim, row] = zero(eltype(s))
-                    idim += 1
+                    idim += 1i32
                 end
             end
         end
         sync_threads()
 
-        # Each thread will compute the influence of all the sources in the shared memory on the target corresponding to its index
-        i = 1
+        # Each thread will compute the influence of all the sources
+        # in the shared memory on the target corresponding to its index
+        i = 1i32
         while i <= bodies_per_col
-            isource = i + bodies_per_col*(col-1)
+            isource = i + bodies_per_col*(col-1i32)
             if isource <= s_size
                 if itarget <= t_size
-                    out .= gpu_interaction(tx, ty, tz, sh_mem, isource, kernel)
-                end
-
-                # Sum up influences for each source in a tile
-                idim = 1
-                while idim <= 12
-                    @inbounds UJ[idim] += out[idim]
-                    idim += 1
+                    gpu_interaction!(UJ, tx, ty, tz, sh_mem, isource, kernel)
                 end
             end
-            i += 1
+            i += 1i32
         end
-        itile += 1
+        itile += 1i32
         sync_threads()
     end
 
     # Sum up accelerations for each target/thread
     # Each target will be accessed by q no. of threads
-    idim = 1
     if itarget <= t_size
-        while idim <= 3
-            @inbounds CUDA.@atomic t[9+idim, itarget] += UJ[idim]
-            idim += 1
+        idim = 1i32
+        while idim <= 3i32
+            @inbounds CUDA.@atomic t[9i32+idim, itarget] += UJ[idim]
+            idim += 1i32
         end
-        idim = 4
-        while idim <= 12
-            @inbounds CUDA.@atomic t[12+idim, itarget] += UJ[idim]
-            idim += 1
+        idim = 4i32
+        while idim <= 12i32
+            @inbounds CUDA.@atomic t[12i32+idim, itarget] += UJ[idim]
+            idim += 1i32
         end
     end
     return
